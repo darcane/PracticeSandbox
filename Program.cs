63d261c19d525ef2679c20a442763154aff25770
Erdem Darcan <erdemdarcan@hotmail.com.tr>
2020-11-04 00:05:23 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PracticeSandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# PracticeSandbox
+ Daily practice :)
